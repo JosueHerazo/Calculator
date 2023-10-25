09cadea899c8a7dd8647d19d6b782efb52e44079
JosueHerazo <136448637+JosueHerazo@users.noreply.github.com>
2023-10-24 22:49:06 -1200

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,5 @@
+const botonNumeros = document.getElementsByName ('data-number');
+const botonOpera = document.getElementsByClassName ( 'dataOpera');
+const botonIgual = document.getElementsByClassName('data-igual')[0];
+const botonDelete = document.getElementsByName('data-delete')[0];
+var result = document.getElementById('result')
